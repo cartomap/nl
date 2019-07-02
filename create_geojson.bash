4e508df94acb5e123cebd149a85835581073ca5e
Edwin de Jonge <edwindjonge@gmail.com>
2019-07-02 15:30:13 +0200

    travis

--- create_geojson.bash
+++ create_geojson.bash
@@ -3,7 +3,8 @@ mkdir -p wgs84
 
 # REGIONNAME="gemeente"
 # YEAR=2016
-YEAR_NOW=date +%Y
+MAPSHAPER=./node_modules/mapshaper/bin/mapshaper
+YEAR_NOW=$(date +%Y)
 
 MDFILE="index.md"
 echo -e "# Contents\n" > $MDFILE
@@ -11,7 +12,7 @@ echo -e "# Contents\n" > $MDFILE
 for REGIONNAME in "provincie" "coropgebied" "gemeente" "wijk" "buurt" "arbeidsmarktregio"
 do
   echo -e "## $REGIONNAME \n" >> $MDFILE
-  for YEAR in {2018..2003}
+  for YEAR in $(seq $YEAR_NOW -1 2003)
   do 
     echo -e "${YEAR}:" >> $MDFILE
     REGION="${REGIONNAME}_${YEAR}"
@@ -21,9 +22,9 @@ do
     test ! -f "wgs84/${REGION}.json" && \
       (curl "http://geodata.nationaalgeoregister.nl/cbsgebiedsindelingen/wfs?request=GetFeature&service=WFS&version=2.0.0&typeName=cbs_${REGION}_gegeneraliseerd&outputFormat=json&SRSName=urn:x-ogc:def:crs:EPSG:4326" > "wgs84/${REGION}.json" \
       || continue)
-    mapshaper "wgs84/$REGION.json" -proj wgs84 -o force "wgs84/$REGION.json" 
-    mapshaper "wgs84/$REGION.json" -simplify 10% keep-shapes -o "wgs84/$REGION.geojson" id-field=statcode precision=0.001 
-    mapshaper "wgs84/$REGION.json" -simplify 10% keep-shapes -o "wgs84/$REGION.topojson" id-field=statcode precision=0.001
+    $MAPSHAPER "wgs84/$REGION.json" -proj wgs84 -o force "wgs84/$REGION.json" 
+    $MAPSHAPER "wgs84/$REGION.json" -simplify 10% keep-shapes -o "wgs84/$REGION.geojson" id-field=statcode precision=0.001 
+    $MAPSHAPER "wgs84/$REGION.json" -simplify 10% keep-shapes -o "wgs84/$REGION.topojson" id-field=statcode precision=0.001
 
     echo "[wgs84,geojson](wgs84/$REGION.geojson)" >> $MDFILE
     echo "[wgs84,topojson](wgs84/$REGION.topojson)" >> $MDFILE
@@ -32,8 +33,8 @@ do
     test ! -f "rd/${REGION}.json" && \
       (curl "http://geodata.nationaalgeoregister.nl/cbsgebiedsindelingen/wfs?request=GetFeature&service=WFS&version=2.0.0&typeName=cbs_${REGION}_gegeneraliseerd&outputFormat=json" > "rd/${REGION}.json" \
       || continue)
-    mapshaper "rd/$REGION.json" -simplify 10% keep-shapes -o "rd/$REGION.geojson" id-field=statcode precision=1
-    mapshaper "rd/$REGION.json" -simplify 10% keep-shapes -o "rd/$REGION.topojson" id-field=statcode precision=1
+    $MAPSHAPER "rd/$REGION.json" -simplify 10% keep-shapes -o "rd/$REGION.geojson" id-field=statcode precision=1
+    $MAPSHAPER "rd/$REGION.json" -simplify 10% keep-shapes -o "rd/$REGION.topojson" id-field=statcode precision=1
     echo "[rd,geojson](rd/$REGION.geojson)" >> $MDFILE
     echo "[rd,topojson](rd/$REGION.topojson)" >> $MDFILE
     echo "" >> $MDFILE
--- index.md
+++ index.md
@@ -1,55 +1,11 @@
-# Contents
+-e # Contents
 
-## provincie 
+-e ## provincie 
 
-2018:
-[wgs84,geojson](wgs84/provincie_2018.geojson)
-[wgs84,topojson](wgs84/provincie_2018.topojson)
-[rd,geojson](rd/provincie_2018.geojson)
-[rd,topojson](rd/provincie_2018.topojson)
+-e 2019:
+[wgs84,geojson](wgs84/provincie_2019.geojson)
+[wgs84,topojson](wgs84/provincie_2019.topojson)
+[rd,geojson](rd/provincie_2019.geojson)
+[rd,topojson](rd/provincie_2019.topojson)
 
-2017:
-[wgs84,geojson](wgs84/provincie_2017.geojson)
-[wgs84,topojson](wgs84/provincie_2017.topojson)
-[rd,geojson](rd/provincie_2017.geojson)
-[rd,topojson](rd/provincie_2017.topojson)
-
-2016:
-[wgs84,geojson](wgs84/provincie_2016.geojson)
-[wgs84,topojson](wgs84/provincie_2016.topojson)
-[rd,geojson](rd/provincie_2016.geojson)
-[rd,topojson](rd/provincie_2016.topojson)
-
-2015:
-[wgs84,geojson](wgs84/provincie_2015.geojson)
-[wgs84,topojson](wgs84/provincie_2015.topojson)
-[rd,geojson](rd/provincie_2015.geojson)
-[rd,topojson](rd/provincie_2015.topojson)
-
-2014:
-[wgs84,geojson](wgs84/provincie_2014.geojson)
-[wgs84,topojson](wgs84/provincie_2014.topojson)
-[rd,geojson](rd/provincie_2014.geojson)
-[rd,topojson](rd/provincie_2014.topojson)
-
-2013:
-[wgs84,geojson](wgs84/provincie_2013.geojson)
-[wgs84,topojson](wgs84/provincie_2013.topojson)
-[rd,geojson](rd/provincie_2013.geojson)
-[rd,topojson](rd/provincie_2013.topojson)
-
-2012:
-[wgs84,geojson](wgs84/provincie_2012.geojson)
-[wgs84,topojson](wgs84/provincie_2012.topojson)
-[rd,geojson](rd/provincie_2012.geojson)
-[rd,topojson](rd/provincie_2012.topojson)
-
-2011:
-[wgs84,geojson](wgs84/provincie_2011.geojson)
-[wgs84,topojson](wgs84/provincie_2011.topojson)
-[rd,geojson](rd/provincie_2011.geojson)
-[rd,topojson](rd/provincie_2011.topojson)
-
-2010:
-[wgs84,geojson](wgs84/provincie_2010.geojson)
-[wgs84,topojson](wgs84/provincie_2010.topojson)
+-e 2018:
