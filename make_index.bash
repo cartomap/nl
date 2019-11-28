44772d0e4557f75abf0d9de0c2ad9a1f137b559a
Edwin de Jonge <edwindjonge@gmail.com>
2019-11-28 08:02:14 +0100

    updated make_index.bash

--- make_index.bash
+++ make_index.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-TYPES=`cat shapes.txt | sed 's/cbs_//' | sed 's/_.*//' | uniq`
+TYPES=`cat shapes.txt | sed 's/cbs_//' | sed 's/_[12].*//' | uniq`
 #echo $TYPES
 for type in $TYPES
 do
   echo ""
-  echo "## $type"
+  echo "# $type"
   echo ""
 
-  SHAPES=$(ls build/wgs84/*.geojson | grep "/$type" -)
+  SHAPES=$(ls -A build/wgs84/*.geojson | grep "/${type}_[12]" -)
   if [ "$SHAPES" == "" ]; then
     continue
   fi
@@ -20,12 +20,24 @@ do
   do
     url=${shape/build/.}
     year=${shape%.*}
-    year=${year#*_}
+    year=${year##*_}
     echo -n " [$year]($url)"
   done
   echo ""
 
-  SHAPES=$(ls build/rd/*.geojson | grep $type -)
+  SHAPES=$(ls build/wgs84/*.topojson | grep $type -)
+  echo -n " - topojson: "
+  
+  for shape in $SHAPES
+  do
+    url=${shape/build/.}
+    year=${shape%.*}
+    year=${year##*_}
+    echo -n " [$year]($url)"
+  done
+  echo ""
+
+  SHAPES=$(ls -A build/rd/*.geojson | grep "/$type" -)
   if [ "$SHAPES" == "" ]; then
     continue
   fi
@@ -38,7 +50,19 @@ do
   do
     url=${shape/build/.}
     year=${shape%.*}
-    year=${year#*_}
+    year=${year##*_}
+    echo -n " [$year]($url)"
+  done
+  echo ""
+
+  SHAPES=$(ls build/rd/*.topojson | grep $type -)
+  echo -n " - topojson: "
+  
+  for shape in $SHAPES
+  do
+    url=${shape/build/.}
+    year=${shape%.*}
+    year=${year##*_}
     echo -n " [$year]($url)"
   done
   echo ""
