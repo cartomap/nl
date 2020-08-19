b465d61791f769ec2b1f4c9fc01b7bf07dc3fa32
Edwin de Jonge <edwindjonge@gmail.com>
2020-08-19 10:16:56 +0200

    adding woonplaats

---
+++ woonplaats.bash
@@ -0,0 +1,13 @@
+curl 'https://geodata.nationaalgeoregister.nl/bag/wfs/v1_1?request=GetFeature&service=WFS&version=1.1.0&typeName=bag:woonplaats&outputFormat=application/json' \
+  -H 'Connection: keep-alive' \
+  -H 'DNT: 1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+  -H 'Sec-Fetch-Site: none' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Accept-Language: en-US,en;q=0.9,nl;q=0.8,fr;q=0.7' \
+  --compressed \
+  -o woonplaats.json
